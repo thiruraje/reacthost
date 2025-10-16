b89e5247b81d7df5ef1e6ea3467141eed9758366
Thiruvengadam Loganathan <42507977+thiruraje@users.noreply.github.com>
2025-10-16 13:47:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import { items } from "./data.js";
 const app = express();
 app.use(cors());
 
+// Serve React build
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 app.get("/api/items", (req, res) => {
   res.json(items);
 });
